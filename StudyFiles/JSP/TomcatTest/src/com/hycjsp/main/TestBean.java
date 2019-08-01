1bed1f4a7f6cd1d934cf1c144c6e11a5fab6dca7
huangyaochun <huangyaochun@xywy.com>
2019-08-01 14:20:28 +0800

    +实体测试

---
+++ StudyFiles/JSP/TomcatTest/src/com/hycjsp/main/TestBean.java
@@ -0,0 +1,14 @@
+package com.hycjsp.main;
+
+public class TestBean {
+	private String message = "I'm Hychun!";
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
